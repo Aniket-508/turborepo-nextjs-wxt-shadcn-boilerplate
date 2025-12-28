2f1775e53ac93eec3cc9a260b3853172259ac7f7
Aniket-508 <pawaraniket508@gmail.com>
2025-12-28 22:33:46 +0530

    chore: simplify ESLint configuration by removing baseConfig import

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,8 +1,4 @@
-import { baseConfig } from "@repo/eslint-config/base";
 import { reactInternalConfig } from "@repo/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
-export default {
-  ...baseConfig,
-  ...reactInternalConfig,
-};
+export default reactInternalConfig;
